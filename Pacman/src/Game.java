2694e6c06872ddd416bed0a15be507ed4f39cad3
naomijub <jnboeira@outlook.com>
2015-12-13 19:19:27 -0200

    Very basic and similar to based implementation coding

---
+++ Pacman/src/Game.java
@@ -0,0 +1,33 @@
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+
+public class Game extends JFrame{
+
+	public Game() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        setTitle("Pacman");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 420);
+        setLocationRelativeTo(null);
+        setVisible(true);        
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 EventQueue.invokeLater(new Runnable() {
+
+	            @Override
+	            public void run() {
+	                Game ex = new Game();
+	                ex.setVisible(true);
+	            }
+	        });
+	}
+
+}
